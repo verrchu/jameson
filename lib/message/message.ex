879500dea413554b32cf4de6e9d0c265fc2199f2
Evgeniy Terekhin <e_terekhin@wargaming.net>
2020-10-07 11:14:43 +0300

    add message fucntions dialyzer specs

--- lib/message/message.ex
+++ lib/message/message.ex
@@ -1,25 +1,33 @@
 defmodule Jameson.Message do
+  @type message() :: map()
+  @type message_type() :: :regular
+
+  @spec regular?(message()) :: boolean()
   def regular?(%{"message" => _msg}), do: true
   def regular?(_msg), do: false
 
+  @spec get_chat_id(message()) :: integer()
   def get_chat_id(msg) do
     cond do
       regular?(msg) -> get_chat_id(:regular, msg)
     end
   end
 
+  @spec get_chat_id(message_type(), message()) :: integer()
   defp get_chat_id(:regular, msg) do
     %{"message" => %{"chat" => %{"id" => chat_id}}} = msg
 
     chat_id
   end
 
+  @spec get_text(message()) :: String.t()
   def get_text(msg) do
     cond do
       regular?(msg) -> get_text(:regular, msg)
     end
   end
 
+  @spec get_text(message_type(), message()) :: String.t()
   defp get_text(:regular, msg) do
     %{"message" => %{"text" => text}} = msg
 
